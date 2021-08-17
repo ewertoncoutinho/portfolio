f5709a299bcee41213c8a27a22c3df2c78774550
ewertoncoutinho <ewertonmacielcoutinho@gmail.com>
2021-08-17 16:40:11 -0300

    upgrade

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	testEnvironment: "jsdom",
-	testPathIgnorePatterns: ["/node_modules/", "/.next/"],
-	collectCoverage: true,
-	collectCoverageFrom: ["pages/**/*.js(x)"],
-	setupFilesAfterEnv: ["<rootDir>/.jest/setup.js"]
-}
