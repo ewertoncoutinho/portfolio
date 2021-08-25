0b15300db5d96a0835f5a0d0ed1b13ef704b53de
ewertoncoutinho <ewertonmacielcoutinho@gmail.com>
2021-08-24 22:47:34 -0300

    works

---
+++ src/components/Works/index.jsx
@@ -0,0 +1,9 @@
+export const Works = () => {
+    return(
+        <section className={["works"].join(' ')}>
+            <div className={["container"].join(' ')}>
+                <h1>Get to know<br></br>my works.</h1>
+            </div>
+        </section>
+    )
+}
