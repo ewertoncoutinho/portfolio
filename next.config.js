d22d6dbc87a17914c24ad3b28ebbfe04093c3145
ewertoncoutinho <ewertonmacielcoutinho@gmail.com>
2022-07-30 20:15:42 -0300

    add analize

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 const { i18n } = require('./next-i18next.config')
-
-module.exports = {
-	i18n,
-	reactStrictMode: true,
-}
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+	enabled: process.env.ANALYZE === 'true',
+})
+module.exports = withBundleAnalyzer({ i18n, reactStrictMode: true })
